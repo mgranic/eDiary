5525c4431b19de941ce3434bd6c61a6da9a565ed
mgranic <mgranic11@gmail.com>
2024-02-03 21:21:08 +0100

    Initial commit
    clean SwiftUI iOS project

---
+++ eDiary_iOS/eDiary_iOS/eDiary_iOSApp.swift
@@ -0,0 +1,32 @@
+//
+//  eDiary_iOSApp.swift
+//  eDiary_iOS
+//
+//  Created by Mate Granic on 03.02.2024..
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct eDiary_iOSApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
