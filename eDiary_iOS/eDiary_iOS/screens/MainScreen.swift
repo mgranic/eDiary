15bd693229c14232b5823778d14e3012df5d3e43
mgranic <mgranic11@gmail.com>
2024-02-24 08:58:50 +0100

    MainScreen basics
    - empty list of chapters
    - emtpy toolbar
    - create chapter button that does nothing
    - add chapter manager
    - get list of dummy Chapters to show on main screen

--- eDiary_iOS/eDiary_iOS/screens/MainScreen.swift
+++ eDiary_iOS/eDiary_iOS/screens/MainScreen.swift
@@ -8,14 +8,61 @@
 import SwiftUI
 
 struct MainScreen: View {
+    @StateObject var chapterManager = ChapterManager()
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Button(action: {
+                    // show sheet to create new chapter
+                }) {
+                    Text("Add chapter")
+                        .font(.system(.title2, design: .rounded))
+                    Image(systemName: "plus.circle.fill")
+                }
+                Text("Expense list")
+                    .font(.system(.title2, design: .rounded))
+                    .foregroundColor(.purple)
+                List {
+                    ForEach(chapterManager.chapterList) { chapter in
+                        HStack {
+                            Text("\(chapter.name)")
+                                .font(.title3)
+                            Spacer()
+                            Text("\(chapter.date)")
+                                .font(.title3)
+                        }
+                        .contentShape(Rectangle())
+                        .onTapGesture {
+                            // navigate to ChapterDetailsScreen
+                            // for navigation add navigation link or use this to show sheet
+                        }
+                    }
+                }
+                .listStyle(.inset)
+            }
+            .onAppear(perform: {
+                // initialize code needed for this screen to displaz properly
+            })
+            .padding()
         }
-        .padding()
+        .toolbar {
+            //NavigationLink(destination: SearchView(modelCtx: modelCtx)) {
+            //    Image(systemName: "magnifyingglass")
+            //}
+            //Menu {
+            //    NavigationLink(destination: ExpenseStatsView()) {
+            //        Text("Expense Stats")
+            //    }
+            //    NavigationLink(destination: ScheduleExpenseView()) {
+            //        Text("Schedule expense")
+            //    }
+            //    NavigationLink(destination: SettingsView()) {
+            //        Text("Settings")
+            //    }
+            //} label: {
+            //    Label("Menu", systemImage: "ellipsis.circle")
+            //}
+        }
     }
 }
 
---
+++ eDiary_iOS/eDiary_iOS/viewmodel/ChapterManager.swift
@@ -0,0 +1,21 @@
+//
+//  ChapterManager.swift
+//  eDiary_iOS
+//
+//  Created by Mate Granic on 24.02.2024..
+//
+
+import Foundation
+
+class ChapterManager : ObservableObject {
+    @Published var chapterList: [Chapter] = [
+        Chapter(name: "chapter 1", date: Date()),
+        Chapter(name: "chapter 2", date: Date()),
+        Chapter(name: "chapter 3", date: Date()),
+        Chapter(name: "chapter 4", date: Date()),
+    ]
+    
+    func getAllChapter() -> [Chapter] {
+        return chapterList
+    }
+}
