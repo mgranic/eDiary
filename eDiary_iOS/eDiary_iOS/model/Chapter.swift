1fba362c46010f355974aa71b9701e83981c163b
mgranic <mgranic11@gmail.com>
2024-04-05 20:10:25 +0200

    Upload chapter
    upload chapter to server without any events. Basically pure HTTP POST request to create chapter on server

--- eDiary_iOS/eDiary_iOS/model/Chapter.swift
+++ eDiary_iOS/eDiary_iOS/model/Chapter.swift
@@ -9,17 +9,33 @@ import Foundation
 import SwiftData
 
 @Model
-class Chapter {
+class Chapter  : Codable {
+    
+    enum CodingKeys: String, CodingKey {
+          case id, userId, name, date, desc
+    }
+    
     var id: UUID
+    var userId: Int?
     var name: String
     var date: Date
     var desc: String
     
-    init(id: UUID = UUID(), name: String, date: Date, description: String) {
+    init(id: UUID = UUID(), userId: Int? = nil, name: String, date: Date, description: String) {
         self.id = id
         self.name = name
         self.date = date
         self.desc = description
+        self.userId = userId
+    }
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = try container.decode(UUID.self, forKey: .id)
+        self.userId = try container.decode(Int.self, forKey: .userId)
+        self.name = try container.decode(String.self, forKey: .name)
+        self.desc = try container.decode(String.self, forKey: .desc)
+        self.date = try container.decode(Date.self, forKey: .date)
     }
     
     // return FetchDescriptor to filter chapter with exact ID
@@ -29,4 +45,13 @@ class Chapter {
         }
         return FetchDescriptor<Chapter>(predicate: predicate)
     }
+    
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id, forKey: .id)
+        try container.encode(userId, forKey: .userId)
+        try container.encode(name, forKey: .name)
+        try container.encode(date, forKey: .date)
+        try container.encode(desc, forKey: .desc)
+      }
 }
--- eDiary_iOS/eDiary_iOS/viewmodel/UploadManager.swift
+++ eDiary_iOS/eDiary_iOS/viewmodel/UploadManager.swift
@@ -18,9 +18,68 @@ class UploadManager {
     
     func uploadChapter() {
         print(chapter.name)
+        sendChapterDataToServer()
         
         for event in eventList {
             print(event.name)
         }
+    }
+    
+    /****************************************************** PRIVATE FUNCTIONS *****************************************************************/
+    private func sendChapterDataToServer() {
+        // Create a URL object for the GET request
+        let url = URL(string: "http://192.168.1.80:5015/chapter/createChapter")!
+        
+        let dateFormatter: DateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-ddTHH:mm:ss.SSSSSSSSSZ"
+        
+        // Create a URLRequest object
+        var request = URLRequest(url: url)
+        
+        // Set the HTTP method to GET
+        request.httpMethod = "POST"
+        
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        let jsonObject: [String: Any] = ["id": 1, "userId": chapter.userId ?? 1, "name": chapter.name, "desc": chapter.desc, "date": dateFormatter.string(from: chapter.date)]
+        //let jsonObject: [String: Any] = ["id": 1, "userId": chapter.userId ?? 1, "name": chapter.name, "desc": chapter.desc, "date": "2024-04-15T13:09:02.596000000"]
+        let jsonData = try! JSONSerialization.data(withJSONObject: jsonObject, options: [])
+        print(String(data: jsonData, encoding: .utf8))
+        request.httpBody = jsonData
+        
+        //let jsonEncoder = JSONEncoder()
+        //let chapterJson = try! jsonEncoder.encode(chapter)
+        //let chapterJsonString = String(data: chapterJson, encoding: .utf8)
+        //print(chapterJsonString)
+        //request.httpBody = chapterJson
+        
+        //let jsonData = try! JSONSerialization.data(withJSONObject: chapter, options: [])
+        //request.httpBody = jsonData
+        
+        // Make the HTTP request
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            // Check for errors
+            if let error = error {
+                print(error)
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse else { return }
+            
+            // Check the response status code
+            if response.statusCode != 200 {
+                print("*****************************************************************")
+                print("HTTP status code: \(response.statusCode)")
+                print("*****************************************************************")
+                return
+            }
+            
+            // Get the response data
+            print(String(data: data!, encoding: .utf8)!)
+            //motivationalQuote = String(data: data!, encoding: .utf8)!
+            //let value = responseString.substring(from: 9)
+            //let x = data!
+            //print(motivationalQuote)
+            
+        }.resume()
     }
 }
